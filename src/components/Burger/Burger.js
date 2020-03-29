e1593014f878ff861506bc73ce9f91af6f3981be
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-03-29 20:51:37 -0300

    Agrega ingredientes con sus estilos respectivos

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top"/>
+      <BurgerIngredient type="cheese"/>
+      <BurgerIngredient type="meat"/>
+      <BurgerIngredient type="salad"/>
+      <BurgerIngredient type="bacon"/>
+      <BurgerIngredient type="bread-bottom"/>
+    </div>
+  );
+};
+
+export default burger;
