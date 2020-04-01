c09fdc7ed0414d812ee5a7d01634282dddd86840
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-03-31 23:16:26 -0300

    Added controls for adding and removing ingredients, not functional yet

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+  let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
     return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
       return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please, start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
