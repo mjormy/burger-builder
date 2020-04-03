8005f383c218229aed64bbd82ab1f6a0b6954662
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-02 22:50:10 -0300

    Al hacer click en el botón Order, muestra una modal con el OrderSummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -20,7 +22,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    checkoutClicked: false
   }
 
   updatePurchaseState(ingredients) {
@@ -70,6 +73,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  checkoutHandler = () => {
+    this.setState({checkoutClicked: true});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -80,12 +87,17 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        
+        <Modal show={this.state.checkoutClicked}>
+          <OrderSummary ingredients={this.state.ingredients}/>
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
+          clicked={this.checkoutHandler}
           price={this.state.totalPrice} />
       </Aux>
     );
