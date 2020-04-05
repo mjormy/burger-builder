81ef9248d63d13fe35b034b4cbff85589a53869e
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-04 21:01:07 -0300

    Agrega un Backdrop al sidebar que desaparece al hacer click con una transición

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        isSideDrawerVisible: true
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({isSideDrawerVisible: false});
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar closed={this.sideDrawerClosedHandler} />
+                <SideDrawer 
+                    open={this.state.isSideDrawerVisible}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+} 
+
+export default Layout;
