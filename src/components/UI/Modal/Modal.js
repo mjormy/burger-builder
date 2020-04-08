d67b3a7b25d6f03d7d066f883cd167a8f3cdd2d5
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-07 22:54:09 -0400

    Adding global error handling with withErrorHandler in BurgerBuilder

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -14,21 +14,21 @@ class Modal extends Component {
     console.log('Modal will update');
   }
 
-  render () {
+  render() {
     return (
       <Aux>
-    <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: this.props.show ? '1' : '0'
-      }}>
-      {this.props.children}
-    </div>
-  </Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
     );
   }
-} 
+}
 
 export default Modal;
