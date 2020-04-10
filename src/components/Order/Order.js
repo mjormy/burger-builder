6259de37ef00387bb696b45abbad5dd073641c4d
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-10 18:04:19 -0400

    Displaying orders from database with ingredients and price

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,35 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients: </p>
-    <p>Price: <strong>price</strong></p>
-  </div>
-);
+const order = (props) => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        name: ingredientName, 
+        amount:props.ingredients[ingredientName]
+      });
+  }
+
+  const ingredientOutput = ingredients.map(ing => {
+    return (<span 
+      style={{
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0 8px',
+        border: '1px solid #ccc',
+        padding: '5px'
+      }}
+      key={ing.key}>{ing.name} ({ing.amount}) </span>);
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>{props.price.toFixed(2)}</strong></p>
+    </div>
+  );
+
+};
 
 export default order;
