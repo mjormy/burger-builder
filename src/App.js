9285f93ff555b942f4697bdca6e8949400e572c9
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-10 17:16:32 -0400

    Include Orders page and use NavLinks to navigate between Burger Builder and Orders

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
 
@@ -12,8 +13,9 @@ class App extends Component {
       <div>
         <Layout>
           <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
             <Route path="/" exact component={BurgerBuilder}/>
-            <Route path="/checkout" component={Checkout}/>
           </Switch>
         </Layout>
       </div>
