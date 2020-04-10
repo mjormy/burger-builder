565e7224188e5c94c0271e8c7dc871570a68da70
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-10 11:20:50 -0400

    Added Checkout summary under BurgerBuilder, no routing yet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <BurgerBuilder />
         </Layout>
+        <Checkout />
       </div>
     );
   }
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.module.css';
+
+const checkoutSummary = (props) => {
+  return (
+    <div className="ChechoutSummary">
+      <h1>We hope you enjoy your burger!</h1>
+      <div style={{ width: '100%', margin: 'auto', textAlign: 'center'}}>
+        <Burger ingredients={props.ingredients} />
+        <Button
+          btnType="Danger"
+          clicked>CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked>CONTINUE</Button>
+      </div>
+    </div>
+  );
+}
+
+export default checkoutSummary;
